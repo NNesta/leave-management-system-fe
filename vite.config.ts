1fca3446b92f149dc92f2c75d24017d7dc462572
Nestor Ngabonziza <ngabonest@gmail.com>
2025-04-23 21:42:14 +0200

    feat: configuring vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: Number(process.env.VITE_PORT) || 4000,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      host: "::",
+      port: Number(env.VITE_PORT) || 4000,
     },
-  },
-}));
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+      Boolean
+    ),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    define: {
+      __APP_NAME__: JSON.stringify(env.VITE_APP_NAME),
+    },
+  };
+});
